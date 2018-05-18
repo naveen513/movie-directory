8bd2d4f5ab2f708d74fe5bdb487f3bae358eef71
Naveen Kumar Uppuluri <naveenupl@gmail.com>
2018-05-18 13:11:54 -0700

    added new movies

--- src/app/shared/services/movies.service.helper.ts
+++ src/app/shared/services/movies.service.helper.ts
@@ -1,5 +1,10 @@
 export const movies = [
   {
+    title: '24 Hours to Live',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/204057.jpg',
+    location: 'Blue'
+  },
+  {
     title: 'A Bad Moms Christmas',
     image: 'https://images.redbox.com/Images/EPC/boxartvertical/203362.jpg',
     location: 'Leela'
@@ -25,8 +30,305 @@ export const movies = [
     location: 'Praneeth'
   },
   {
+    title: 'Beyond Skyline',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/204090.jpg',
+    location: 'Blue'
+  },
+  {
     title: 'Blade Runner 2049',
     image: 'https://images.redbox.com/Images/EPC/boxartvertical/201817.jpg',
     location: 'Praneeth'
+  },
+  {
+    title: 'Daddys Home 2',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202861.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Dead Draw',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/204505.jpg',
+    location: 'Blue'
+  },
+  {
+    title: 'Despicable Me 3',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202778.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Downsizing',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202864.jpg',
+    location: 'Blue'
+  },
+  {
+    title: 'Doctor Strange',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200105.jpg',
+    location: 'Usha - EX'
+  },
+  {
+    title: 'Dunkirk',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200326.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'Finding Dory',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/8170.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'Flatliners',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200335.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Geostorm',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202781.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Girls Trip',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200423.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Happy Death Day',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202835.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Harry Potter and the Sorcerers Stone',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/3241.jpg',
+    location: '2001'
+  },
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/8700.jpg',
+    location: '2002'
+  },
+  {
+    title: 'Harry Potter and the Prisoner of Azkaban',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/2024.jpg',
+    location: '2004'
+  },
+  {
+    title: 'Harry Potter and the Goblet of Fire',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/5716.jpg',
+    location: '2005'
+  },
+ /*
+  {
+    title: 'Harry Potter and the Order of the Phoenix',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/5072.jpg',
+    location: '2007-NA'
+  },
+  {
+    title: 'Harry Potter and the Half Blood Prince',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/2095.jpg',
+    location: '2009-NA'
+  },
+  {
+    title: 'Harry Potter and the Deathly Hallows: Part 1',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/5545.jpg',
+    location: '2010-NA'
+  },
+  {
+    title: 'Harry Potter and the Deathly Hallows: Part 2',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/6507.jpg',
+    location: '2011-NA'
+  },
+  */
+  {
+    title: 'It (2017)',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200894.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Jumanji: Welcome To The Jungle',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200276.jpg',
+    location: 'Blue'
+  },
+  {
+    title: 'Jungle',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203335.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Justice League',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202770.jpg',
+    location: 'blue'
+  },
+  {
+    title: 'Kidnap',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200132.jpg',
+    location: 'praneeth'
+  },
+  {
+    title: 'Kingsman: The Golden Circle',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200305.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'Kingsman: The Secret Service',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/7490.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Leap!',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200884.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Logan Lucky',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/201990.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'Mad Max: Fury Road',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/7556.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Marshall',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203263.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Guardians Of The Galaxy',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/7217.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Guardians of the Galaxy Vol. 2',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200300.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Moana',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200124.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'Monster Trucks',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/7469.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Office Christmas Party',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200113.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'Only the Brave',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200589.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'The Pirates of Somalia',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/204071.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Pitch Perfect 2',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/7555.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Roman J. Israel, Esq.',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203311.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Rough Night',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200329.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Shape of Water',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203251.jpg',
+    location: 'Blue'
+  },
+  {
+    title: 'The Star',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203310.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Star Wars: The Last Jedi',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203216.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'The Foreigner',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203261.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Intern',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/8139.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'The Snowman',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202836.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'The Dark Tower',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200140.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Fate of the Furious',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200304.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Hitmans Bodyguard',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200587.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Legend of Tarzan (2016)',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/8177.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Mountain Between Us',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203224.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Mummy (2017)',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200302.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'The Snowman',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/202836.jpg',
+    location: 'Blue'
+  },
+  {
+    title: 'Thor: Ragnarok',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203214.jpg',
+    location: 'Blue'
+  },
+  {
+    title: 'Tyler Perrys Boo 2! A Madea Halloween',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203266.jpg',
+    location: 'Leela'
+  },
+  {
+    title: 'Valerian and the City of a Thousand Planets',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200336.jpg',
+    location: 'Usha'
+  },
+  {
+    title: 'Victoria and Abdul',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/203254.jpg',
+    location: 'Praneeth'
+  },
+  {
+    title: 'Wonder Woman (2017)',
+    image: 'https://images.redbox.com/Images/EPC/boxartvertical/200299.jpg',
+    location: 'Usha'
   }
 ];
